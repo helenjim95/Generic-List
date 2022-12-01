e8fed2642e31ec9a8e869659837389547cae43ca
ge32goy <hanyin.chen@tum.de>
2022-12-01 22:03:06 +0100

    commit

--- src/de/tum/in/ase/LinkedList.java
+++ src/de/tum/in/ase/LinkedList.java
@@ -1,9 +1,248 @@
 package de.tum.in.ase;
 
-// TODO: make it implement the interface MyList
-public class LinkedList<T> {
-    // TODO: add attributes
-    // TODO: add a constructor
-    // TODO: add getters and setters
-    // TODO: implement interface methods
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// make it implement the interface MyList
+public class LinkedList<T> implements MyList<T> {
+
+    ListNode<T> node0;
+    ListNode<T> node1;
+    ListNode<T> node2;
+
+    public LinkedList(ListNode<T> node0, ListNode<T> node1, ListNode<T> node2) {
+        this.node0 = node0;
+        this.node1 = node1;
+        this.node2 = node2;
+    }
+
+    public ListNode<T> getNode0() {
+        return node0;
+    }
+
+    public void setNode0(ListNode<T> node0) {
+        this.node0 = node0;
+    }
+
+    public ListNode<T> getNode1() {
+        return node1;
+    }
+
+    public void setNode1(ListNode<T> node1) {
+        this.node1 = node1;
+    }
+
+    public ListNode<T> getNode2() {
+        return node2;
+    }
+
+    public void setNode2(ListNode<T> node2) {
+        this.node2 = node2;
+    }
+
+    @Override
+    public int size() {
+        if (node0.getNext() == null) {
+            return 0;
+        } else {
+            int count = 0;
+            ListNode<T> current = node0;
+            while (current != null) {
+                count += 1;
+                current = current.getNext();
+            }
+            return count;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return node0.getNext() == null;
+    }
+
+    @Override
+    public boolean contains(T o) {
+        if (this.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (this.get(i).equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T t) {
+        if (!this.isEmpty()) {
+            ListNode<T> newNode = new ListNode<>(t, this.getNode2(), null);
+            this.node2.setNext(newNode);
+        } else {
+            ListNode<T> newNode = new ListNode<>(t, null, null);
+        }
+    }
+
+    @Override
+    public void remove(T o) {
+        if (this.contains(o)) {
+//            removes the first node (if it exists) with the value equal to the method parameter.
+            int index = indexOf(o);
+            remove(index);
+        }
+    }
+
+    @Override
+    public void clear() {
+        if (!this.isEmpty()) {
+            node0.setNext(null);
+            node2.setPrevious(null);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (node0.getNext() == null || index > this.size() - 1) {
+            throw new IndexOutOfBoundsException("List index is out of bound");
+        } else if (index == this.size() - 1) {
+            return node2.getValue();
+        } else {
+            int count = 0;
+            ListNode<T> current = node0;
+            while(count < index) {
+                count += 1;
+                current = current.getNext();
+            }
+            return current.getValue();
+        }
+    }
+
+    @Override
+    public void add(int index, T element) {
+        ListNode<T> listNodeElement = new ListNode<>(element);
+        if (index > this.size() - 1) {
+            throw new IndexOutOfBoundsException("List index is out of bound");
+        } else {
+            for (int i = this.size() - 1; i >= index; i--) {
+                if (i == this.size() - 1) {
+                    ListNode<T> newNode = new ListNode<>(element, this.getNode2(), null);
+                    this.node2.setNext(newNode);
+                } else if (i == 0) {
+                    ListNode<T> newNode = new ListNode<>(element, null, this.getNode0());
+                    this.node0.setPrevious(newNode);
+                } else {
+                    ListNode<T> newNode = new ListNode<>(element);
+                    ListNode<T> previousNode = new ListNode<>(this.get(i - 1));
+                    ListNode<T> currentNode = new ListNode<>(this.get(i));
+                    newNode.setPrevious(previousNode);
+                    newNode.setNext(currentNode);
+                    previousNode.setNext(newNode);
+                    currentNode.setPrevious(newNode);
+                }
+            }
+        }
+    }
+
+    @Override
+    public T remove(int index) {
+        return get(index);
+    }
+//        if (index < 0 || index > this.size() - 1) {
+//            throw new IndexOutOfBoundsException("List index is out of bound");
+//        } else if (isEmpty()) {
+//            return null;
+//        } else {
+//            int count = index;
+//            ListNode<T> current = node0;
+//            while (count < this.size() - 1) {
+//                if (count == 0) {
+//                    T node = node0.getValue();
+//                    node0.setNext(null);
+//                    return node;
+//                } else {
+//                    if (count == this.size() - 1) {
+//                        T node = node2.getValue();
+//                        node2.setPrevious(null);
+//                        ListNode<T> previousNode = new ListNode<>(this.get(count - 1));
+//                        previousNode.setNext(null);
+//                        return node;
+//                    } else {
+//                        ListNode<T> previousNode = new ListNode<>(this.get(count - 1));
+//                        ListNode<T> currentNode = new ListNode<>(this.get(count));
+//                        ListNode<T> nextNode = new ListNode<>(this.get(count + 1));
+//                        previousNode.setNext(nextNode);
+//                        nextNode.setPrevious(previousNode);
+//                        return currentNode.getValue();
+//                    }
+//                    count += 1;
+//                    current = current.getNext();
+//                }
+//            }
+//        }
+//    }
+
+//
+//            for (int i = index; i < this.size() - 1; i++) {
+//                if (i == this.size() - 1) {
+//                    T node = node2.getValue();
+//                    node2.setPrevious(null);
+//                    ListNode<T> previousNode = new ListNode<>(this.get(i - 1));
+//                    previousNode.setNext(null);
+//                    return node;
+//                } else if (i == 0) {
+//                    T node = node0.getValue();
+//                    node0.setNext(null);
+//                    return node;
+//                } else {
+//                    ListNode<T> previousNode = new ListNode<>(this.get(i - 1));
+//                    ListNode<T> currentNode = new ListNode<>(this.get(i));
+//                    ListNode<T> nextNode = new ListNode<>(this.get(i + 1));
+//                    previousNode.setNext(nextNode);
+//                    nextNode.setPrevious(previousNode);
+//                    return currentNode.getValue();
+//                }
+//            }
+//            return node;
+
+
+    @Override
+    public int indexOf(T o) {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (this.get(i).equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+//     The return value should consist of the toString() return of every value, separated by ,
+//     (a comma followed by a single space) and encapsulated in brackets: []
+    @Override
+    public String toString() {
+//        Need to change this?
+        List<T> elements = new ArrayList<>();
+        if (isEmpty()) {
+            return "[]";
+        } else {
+            if (size() == 1) {
+                T value = get(0);
+                return value.toString();
+            } else {
+            // iterate through list and add value as string to list
+                int count = 0;
+                ListNode<T> current = node0;
+                while(count < this.size()) {
+                    elements.add(current.getValue());
+                    count += 1;
+                    current = current.getNext();
+                }
+                return "[" + elements.stream().map(String::valueOf).collect(Collectors.joining(",")) + "]";
+            }
+        }
+    }
+
 }
--- src/de/tum/in/ase/ListNode.java
+++ src/de/tum/in/ase/ListNode.java
@@ -1,7 +1,45 @@
 package de.tum.in.ase;
 
 public class ListNode<T> {
-    // TODO: add attributes
-    // TODO: add 3 constructors as in the problem statement
-    // TODO: add getters and setters
+    T value;
+    ListNode<T> previous;
+    ListNode<T> next;
+
+    public ListNode() {
+    }
+
+    public ListNode(T value) {
+        this.value = value;
+    }
+
+
+    public ListNode(T value, ListNode<T> previous, ListNode<T> next) {
+        this.value = value;
+        this.previous = previous;
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public ListNode<T>getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(ListNode<T> previous) {
+        this.previous = previous;
+    }
+
+    public ListNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
 }
